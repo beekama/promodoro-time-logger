1a2acfb531410b897ad4daceafbc4ef35c645598
Kathrin Maurer <k.aus.w@gmx.de>
2026-01-05 23:49:52 +0100

    ref: app to server/main

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from dotenv import load_dotenv
-import server.main as main
-import os
+from server.main import create_app
 
 load_dotenv()
 
-def createApp(environment=None, start_response=None):
-    main.app.secret_key = os.environ["FLASK_SECRET_KEY"]
-    with main.app.app_context():
-        main.create_app()
-    return main.app
+app = create_app()
 
 if __name__ == "__main__":
-    main.app.run(debug=True)
+    app.run(debug=True)
--- server/main.py
+++ server/main.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from routes.projects import projects_bp
 
-app = Flask(__name__)
 
 def create_app():
+    app = Flask(__name__)
+    app.config.from_prefixed_env()
     app.register_blueprint(projects_bp)
     return app
