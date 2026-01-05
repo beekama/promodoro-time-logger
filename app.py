4dd73686aac3da8cc42641f21ae7438ed8878f7b
Kathrin Maurer <k.aus.w@gmx.de>
2026-01-05 23:13:40 +0100

    fix: shift to server

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import server.main as main
+import os
 
 load_dotenv()
 
 def createApp(environment=None, start_response=None):
+    main.app.secret_key = os.environ["FLASK_SECRET_KEY"]
     with main.app.app_context():
         main.create_app()
     return main.app
+
+if __name__ == "__main__":
+    main.app.run(debug=True)
